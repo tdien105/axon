2784b2df210e941524f3711033862eccebbe60c3
Dien Phan <tdien105@Mac-DienPhan.local>
2020-08-29 11:41:37 +0700

    .

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import re,json, requests
+from flask import request, Flask
+
+# global vars
+headers = {'content-type': 'application/json'}
+app = Flask(__name__)
+api_url = os.environ['api_url']
+### end global vars
+
+
+@app.route("/", methods=["POST"])
+def parsejson():
+  link = request.json["json_link"]
+  res = requests.get(link)
+
+  if res.status_code == 200:
+    lines = res.text.splitlines() # search line by line for more accurate
+    for index,line in enumerate(lines):
+      d = {'text': re.sub('[^a-zA-Z0-9 \n\.]', '', line), 'model': 'en'} #remove all special characters before extracting names
+      try:
+        res = requests.post(api_url, data=json.dumps(d), headers=headers)
+      except:
+        return "Error occur when connecting to backend api"
+
+      if res.status_code == 200:
+        r = res.json()
+        for i in r:
+          n = i['text']
+          if i['type'] == "PERSON":
+            line = line.replace(n,'') # remove name from current line
+      else:
+        return "Error occur when connecting to backend api"
+      lines[index] = line # update back to origin text
+    
+    result = json.loads('\n'.join(lines))
+    result['AXON_INTERVIEW'] = "true"
+    return result
+  return "Could not fetch json files"
+
+if __name__=='__main__':
+    app.run(host= '0.0.0.0')
